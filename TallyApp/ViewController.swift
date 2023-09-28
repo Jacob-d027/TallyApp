5f34eb561a0d481d1a16f59feca6a139fbf21ab5
Jacob Davis <jacobdofficial@gmail.com>
2023-09-28 13:54:47 -0600

    Initial Commit

---
+++ TallyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TallyApp
+//
+//  Created by Jacob Davis on 9/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
