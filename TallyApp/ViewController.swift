275f296d229f65cbfd9168e5280c382c3d6a3e51
Jacob Davis <jacobdofficial@gmail.com>
2023-09-29 08:15:57 -0600

    Updated contraints and added some functionality

--- TallyApp/ViewController.swift
+++ TallyApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var interval: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var userInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func counterButton(_ sender: Any) {
+        if let numberToIncrementOrDecrement = Int(interval.text!) {
+            
+        }
+        
+        
+    }
+    
+    @IBAction func setToChangeBy(_ sender: Any) {
+        guard let intervalRate = Int(userInput.text ?? "1") else {return}
+        interval.text = String(intervalRate)
+        
+    }
 }
 
