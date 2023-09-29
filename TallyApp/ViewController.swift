57a9eacaad26ffa7b476105493d98375995eda50
Jacob Davis <jacobdofficial@gmail.com>
2023-09-29 09:09:05 -0600

    FInished adding reset functionality and allows for increasing and decreasing values

--- TallyApp/ViewController.swift
+++ TallyApp/ViewController.swift
@@ -9,27 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var descriptorOfInterval: UILabel!
     @IBOutlet weak var interval: UILabel!
     @IBOutlet weak var counterLabel: UILabel!
     @IBOutlet weak var userInput: UITextField!
+    var countTotal: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        countTotal = 0
+        counterLabel.text = "0"
+        interval.text = "1"
+        descriptorOfInterval.text = "Increasing by:"
+    }
+    
     @IBAction func counterButton(_ sender: Any) {
-        if let numberToIncrementOrDecrement = Int(interval.text!) {
-            
-        }
+    guard let addToCount = Int(interval.text!) else {return}
         
-        
+        countTotal += addToCount
+        counterLabel.text = String(countTotal)
     }
     
     @IBAction func setToChangeBy(_ sender: Any) {
         guard let intervalRate = Int(userInput.text ?? "1") else {return}
-        interval.text = String(intervalRate)
         
+       if intervalRate < 0 {
+            descriptorOfInterval.text = "Decreasing by:"
+        } else {
+            descriptorOfInterval.text = "Increasing by:"
+        }
+        
+        interval.text = String(intervalRate)
     }
 }
 
